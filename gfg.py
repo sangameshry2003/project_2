46b5b1e1a9ac3f4bda8f623c1f45a8d23afe270f
sangamesh <sangameshryankanchi2003@gmail.com>
2023-11-01 10:13:22 +0530

    sangu

---
+++ gfg.py
@@ -0,0 +1,12 @@
+"""
+This module demonstrates basic addition in Python.
+
+It defines two variables A and B, calculates their sum, and prints the result.
+"""
+
+A = 1
+B = 2
+
+SUM_RESULT = A + B
+
+print(SUM_RESULT)
